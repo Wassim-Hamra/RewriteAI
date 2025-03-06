c6614a5b55e3207ce24ffa3177997711c8c7d45c
Wassim <wassimhamraa@gmail.com>
2025-03-06 15:49:25 +0100

    first commit

---
+++ Agents/supervisor.py
@@ -0,0 +1,22 @@
+from langgraph_supervisor import create_supervisor
+from langchain_groq import ChatGroq
+
+model = ChatGroq(model="deepseek-r1-distill-llama-70b")
+
+supervisor = create_supervisor(
+    [Scraper, Rewriter, DB_Writer],
+    model=model,
+    prompt=(
+        "You are the Supervisor Node of a multi-step AI-driven news management system."
+        "Your role is to orchestrate the entire process, ensuring smooth coordination between the components: the News Scraper, News Rewriter, and Database Writer."
+        "Your tasks include initiating and managing the flow of data between these components, handling errors, and ensuring that each stage is completed before proceeding to the next"
+        "Steps:"
+        "Initiate Scraping: Begin by triggering the News Scraper. The scraper will gather news articles from various trusted sources. Ensure that the news data is structured and ready for the next stage."
+        "Rewriting: After the news articles have been scraped, pass the articles to the News Rewriter. The Rewriter will process each article, rephrasing, and restructuring it to ensure the content is unique while maintaining its original meaning and structure. Once the rewriting is completed, ensure the content is properly formatted for database insertion."
+        "Writing to Database: Once the rewritten news is ready, hand it off to the Database Writer. The writer will store the rewritten articles in the database for future retrieval, ensuring that the articles are properly tagged, categorized, and time-stamped."
+        "Error Handling: Continuously monitor for any errors that might arise during the scraping, rewriting, or database writing process. If an error occurs, retry the step that the error occured in."
+        "Requirements:"
+        "You should ensure that each task is completed in order, and no step should start until the previous one is finished successfully."
+        "Handle data flow seamlessly between each component, passing the necessary data (scraped news → rewritten content → database entries)."
+    )
+)
